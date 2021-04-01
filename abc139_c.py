7f9f07915b53d476f47cbe333a9e79ab2105e752
Miyuki Kondo/Abe <meowmeowcats0830v@gmail.com>
2021-04-01 23:00:55 +0900

    1 easy TLE

--- abc139_c.py
+++ abc139_c.py
@@ -1,20 +1,28 @@
 N = int(input())
 A = list(map(int, input().split()))
 
-print(A)
+#print(A)
 
 m = 0 #max
 s = 0 #start_index
+bef = s
 e = s + 1 #end_index
-while s < N: # (0, 5)
-    print(s, e, m)
-    if A[s] >= A[e]:#次に降りたつ # 10 > 4
-        e = s+1
+i = 0
+while e < N: # (0, 5)
+#    print(s, e, bef, e-s,m, A[s], A[bef], A[e])
+    if A[bef] >= A[e]:#次に降りたつ # 10 > 4
+        bef = e
+        e += 1
     else: #次に降りたてない
         if m < e-s-1: 
-            m = e-s-1 
+            m = e-s-1
         s = s + 1 #初期化して次のループへ # 1
+        bef = s
         e = s + 1
+    if i > 1000000:
+        break
+if m < e-s-1: 
+    m = e-s-1
 
 print(m)
 

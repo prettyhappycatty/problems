425face1d3545ae467424c81e7c97bea98bed674
Miyuki Kondo/Abe <meowmeowcats0830v@gmail.com>
2021-04-08 17:59:01 +0900

    1 easy

---
+++ agc037_a.py
@@ -0,0 +1,27 @@
+S = input()
+#print(len(S))
+i = 0
+cnt = 0
+while i < len(S):
+    if i == 0:
+        bef = S[i]
+        cnt = 1
+    else:
+
+        if bef == S[i] and i+1 < len(S):
+            bef = S[i] + S[i+1]
+            cnt += 1
+            i +=1
+        elif bef == S[i] and i+1 == len(S):
+            bef == bef + S[i]
+            cnt = cnt
+        else:
+            bef = S[i]
+            cnt += 1
+
+#    print(i, cnt, bef)
+    i += 1
+print(cnt)
+
+
+    

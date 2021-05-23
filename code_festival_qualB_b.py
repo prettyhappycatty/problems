e7660c1c8ddfa6a9f311e14e7dcca79736b65f83
Miyuki Kondo/Abe <meowmeowcats0830v@gmail.com>
2021-05-23 23:35:19 +0900

    Create code_festival_qualB_b.py

---
+++ code_festival_qualB_b.py
@@ -0,0 +1,13 @@
+N, K = map(int,input().split())
+A = []
+
+for i in range(N):
+    tmp_A = int(input())
+    A.append(tmp_A)
+#print(len(A))
+sum_a = 0
+for i in range(len(A)):
+    sum_a += A[i]
+    if K < sum_a:
+        print(i+1)
+        break

8db5328721f811d89e4da7754cd534574763f272
Miyuki Kondo/Abe <meowmeowcats0830v@gmail.com>
2021-07-20 21:26:39 +0900

    Update abc182_c.py

--- abc182_c.py
+++ abc182_c.py
@@ -1,28 +1,32 @@
 s = input()
 ar = []
-dic = {0:0, 1:0, 2:0}
+dic = {0:0,1:0, 2:0}
 
+all_cnt = 0
 for i in range(len(s)):
     a = int(s[i]) % 3
+    all_cnt += a
+    all_cnt %= 3
     ar.append(a)
-    if a == 0:
-        continue
-    elif a == 1 and dic[2] > 0:
-        dic[2] -= 1 
-    elif a == 2 and dic[1] > 0:
-        dic[1] -= 1
-    else:
-        dic[a] += 1
+    dic[a] += 1
 
+#print(all_cnt)
 #print(ar)
 #print(dic)
 
-cnt = 0
-for k, v in dic.items():
-    if v > 0:
-        cnt += v
-
-if cnt == len(s):
-    print(-1)
-else:
-    print(cnt % 3)
+if all_cnt == 0:
+    print(0)
+elif all_cnt == 1:
+    if dic[1] > 0 and len(s) != 1:#1を取り除く場合
+        print(1)
+    if dic[2] > 1 and len(s) != 2:#2を2個取り除く場合
+        print(2)
+    else:
+        print(-1)
+else:#2余る場合
+    if dic[2] > 0 and len(s) != 1:#2を1個取り除く場合
+        print(1)
+    elif dic[1] > 1 and len(s) != 2:#1を2個取り除く場合
+        print(2)
+    else:
+        print(-1)

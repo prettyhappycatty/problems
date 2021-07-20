50b2739227e63b3debf252249a2f09696d562666
Miyuki Kondo/Abe <meowmeowcats0830v@gmail.com>
2021-07-20 22:45:53 +0900

    Update abc182_c.py

--- abc182_c.py
+++ abc182_c.py
@@ -19,7 +19,7 @@ if all_cnt == 0:
 elif all_cnt == 1:
     if dic[1] > 0 and len(s) != 1:#1を取り除く場合
         print(1)
-    if dic[2] > 1 and len(s) != 2:#2を2個取り除く場合
+    elif dic[2] > 1 and len(s) != 2:#2を2個取り除く場合
         print(2)
     else:
         print(-1)

0b220d1f1bf675db2fddcc2b3e2edc20c92d7991
Miyuki Kondo/Abe <meowmeowcats0830v@gmail.com>
2021-08-05 22:58:56 +0900

    Update diverta2019_b.py

--- diverta2019_b.py
+++ diverta2019_b.py
@@ -1,10 +1,15 @@
-R, G, B, N = list(map(int, input().split()))
+
+
+#r, g, b, n = lstdin.readline().rstrip().split()
+r, g, b, n = list(map(int, input().split()))
 
 cnt = 0 
-for i in range(N//R + 1):
-    for j in range(N//G + 1):
-        k = i * R + j * G
-        if N >= k and (N - k) % B == 0:
+for i in range(n//r+1):
+    red = i * r
+    for j in range(n//g+1):
+        green = j * g
+        if n < red + green:
+            continue
+        elif (n - red - green) % b == 0:
             cnt += 1
- 
 print(cnt)

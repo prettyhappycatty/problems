fce52b28b08bcc62b682957d7b219ba213899f1b
Miyuki Kondo/Abe <meowmeowcats0830v@gmail.com>
2021-09-01 21:23:42 +0900

    Create algo_327.py

---
+++ algo_327.py
@@ -0,0 +1,27 @@
+import math
+
+def eratosthenes():
+	n = 30
+	ret = [i for i in range(1, n+1)]
+	for i in range(1, 4):
+		if ret[i] > 0:
+			j = ret[i]
+			while 1:
+				j += ret[i]
+				if j-1 >= n:
+					break
+				ret[j-1] = -1
+	
+	for i in range(4, int(math.sqrt(30))+1):
+		if ret[i] > 0:
+			j = ret[i]
+			while 1:
+				j += ret[i]
+				if j-1 >= n:
+					break
+				#ret[j-1] = -1
+				if ret[j-1] > -1:
+					return ret[j-1]
+	return ret
+
+print(eratosthenes())

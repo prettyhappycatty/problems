2b504b4b9b0477baf7e866eecbcc09d2d9534903
Miyuki Kondo/Abe <meowmeowcats0830v@gmail.com>
2021-09-05 21:54:30 +0900

    Update abc043_b.py

--- abc043_b.py
+++ abc043_b.py
@@ -3,13 +3,16 @@ s = input()
 ret = ['']*len(s)
 cursor = 0
 for i in range(0, len(s)):
-    print(cursor)
+    #print(cursor)
     if s[i] == 'B':
         cursor -= 1
         ret[cursor] = ''
         ret[i] = ''
+        if cursor < 0:
+            cursor = 0
     else:
-        ret[i] = s[i]
+        ret[cursor] = s[i]
         cursor += 1
+    #print(cursor, ret)
 
 print(''.join(ret))

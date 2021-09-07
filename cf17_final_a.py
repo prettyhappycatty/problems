2e1d4c0cb91cc05cbc367f758da2aacc4c403445
Miyuki Kondo/Abe <meowmeowcats0830v@gmail.com>
2021-09-08 00:41:20 +0900

    Create cf17_final_a.py

---
+++ cf17_final_a.py
@@ -0,0 +1,32 @@
+S = input()
+
+ary = ["KIH", "B", "R"]
+
+tmp = S
+i = 0
+#KIHの前が、"A"か""であること"
+if "KIH" not in tmp:
+    print("NO")
+    exit()
+
+S1 = S.split("KIH")
+
+#Bの前が、"A"か""であること"
+if "B" not in S1[1] or (S1[0] != "A" and S1[0] != ""):
+    print("NO")
+    exit()
+
+S2 = S1[1].split("B")
+
+#Rの前が、"A"か""であること"
+if "R" not in S2[1] or (S2[0] != "A" and S2[0] != ""):
+    print("NO")
+    exit()
+
+S3 = S2[1].split("R")
+#Rのあとが、"A"か""であること"
+if S3[1] != "A" and S3[1] != "":
+    print("NO")
+    exit()
+
+print("YES")

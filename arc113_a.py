d50ffa281360fe1da41660551f60f6210292c9f6
Miyuki Kondo/Abe <meowmeowcats0830v@gmail.com>
2021-09-10 22:52:36 +0900

    Update arc113_a.py

--- arc113_a.py
+++ arc113_a.py
@@ -1,7 +1,7 @@
 K = int(input())
 ans = 1
-for a in range(1,K):
-    for b in range(1, -(-K//a)+1):
+for a in range(1,K): #AはKより小さい
+    for b in range(1, K//a+1): #BはK//aより小さい
         c = K//(a*b)
         ans += c
 

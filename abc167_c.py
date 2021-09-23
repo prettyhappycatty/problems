250eabb05f010bbda971fc4d3ca6016e7112110e
Miyuki Kondo/Abe <meowmeowcats0830v@gmail.com>
2021-09-23 21:29:44 +0900

    Update abc167_c.py

--- abc167_c.py
+++ abc167_c.py
@@ -20,38 +20,46 @@ buy_list = bit_list(N) #N冊の本を買うか買わないか
 
 book = []
 
-for i in range(M): #本の情報を記録
+for i in range(N): #本の情報を記録
     A = list(map(int, input().split()))
     book.append(A)
-print(book)
+#print(book)
 
 ans = []
+costs = []
+smallest = 10**10
+
 for j in range(len(buy_list)):#bit探索の1パターン
-    AM = [ 0 for _ in range(M)] #M個のアルゴリズムのスキル
+    AM = [0 for _ in range(M)] #M個のアルゴリズムのスキル
+    cost = 0
     for k in range(N):#本の個数
         for i in range(M):#A[0]はコストのため、M個のアルゴリズムの分をたす
-            #print(book[int(buy_list[j][k])][k+1], int(buy_list[j][i]))
-            AM[i] += book[int(buy_list[j][k])][i+1]*int(buy_list[j][k]) #jを買う時、iのアルゴリズムの伸び
+            #print(i, book[k][i+1], int(buy_list[j][k]))
+            AM[i] += book[k][i+1]*int(buy_list[j][k]) #jを買う時、iのアルゴリズムの伸び
+        cost += book[k][0]*int(buy_list[j][k]) 
+
+    costs.append(cost)
     ans.append(AM)
 
-print(ans)
+#print(ans)
+#print(costs)
 
 for j in range(len(buy_list)):
     flg = 0
-    cost = 0
-    smallest = 10**10
     for i in range(M):
-        cost += int(book[i][0])
-        print(ans[j][i])
         if ans[j][i] < X:#一つでもX未満であれば
             flg = 1
             break
     if flg == 1: #次のパターンへ
         continue
     else:
-        print(smallest, cost)
-        smallest = min(smallest,cost)
+        #print(smallest, costs[j])
+        smallest = min(smallest,costs[j])
 
-print(smallest)
+#print(N,M, X)
+if smallest == 10**10:
+    print(-1)
+else:
+    print(smallest)
 
 

13ea4f1ce7ac623f51077e0b842f512a25ae5534
Miyuki Kondo/Abe <meowmeowcats0830v@gmail.com>
2021-11-27 15:21:19 +0900

    Update abc218_d.py

--- abc218_d.py
+++ abc218_d.py
@@ -1,15 +1,25 @@
 # 長方形の数　N個のvertexから4つを選ぶ
 
-N = int(input())
+def solve():
+    #N = int(input())
+    N = 2000
+    # list だと通らない（TLE）
+    #p = set([tuple((i,i)) for i in range(2000)])
+    p = [(i,i) for i in range(2000)]
 
-# list だと通らない（TLE）
-p = set([tuple(input().split()) for _ in range(N)])
+    start = time.time()
+    #print(p)
+    cnt = 0
+    for i,j in p:
+        for k,l in p:
+            if i != k and j != l and (i,l) in p and (k,j) in p:
+                cnt += 1
 
-print(p)
-cnt = 0
-for i,j in p:
-    for k,l in p:
-        if i != k and j != l and (i,l) in p and (k,j) in p:
-            cnt += 1
+    print(cnt // 4)
+    elapsed_time = time.time() - start
+    print ("elapsed_time:{0}".format(elapsed_time) + "[sec]")
 
-print(cnt // 4)
+import time
+
+if __name__ == '__main__':
+    solve()
